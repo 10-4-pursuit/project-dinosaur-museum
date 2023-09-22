e6022b2094a510fc9b5ad35bdb3067cd1bf0421e
SaraM09 <sayeramusfika@pursuit.org>
2023-09-22 19:11:53 -0400

    created a receipt variable to build out each line in the receipt string. Each line reflects the information in each purchase object within the Purchase array of objects. This version of the receipt variable takes into account if there are no extras in the extras array for the key extras in the purchase object within the Purchases array of object

--- src/03-ticket-calculator.js
+++ src/03-ticket-calculator.js
@@ -149,9 +149,12 @@ for(let i = 0; i < purchases.length; i++){
     capitalizedTicketType = purchases[i].ticketType[0].toUpperCase() + purchases[i].ticketType.slice(1)
     // created a variable to contain the formatted version of each extra in the Extras array in the purchase object which is the value for the key value pair for the extras key in the purchase object in the purchases array of objects. Capitalized the first letter of each extra in the extras array and used slice method to ad the rest of the word. Then used join to join the string values in the array into a string.
     formattedExtras = purchases[i].extras.map(extra => extra[0].toUpperCase() + extra.slice(1) + ' Access').join(", ")
+    // created an if statement to check to see if there are any values within the extras arraay which is a value for the key extra in the purchase object within the Purchases array of objects
     if(purchases[i].extras.length === 0){
+    // created a receipt variable to build out each line in the receipt string. Each line reflects the information in each purchase object within the Purchase array of objects. This version of the receipt variable takes into account if there are no extras in the extras array for the key extras in the purchase object within the Purchases array of objects
       receipt += `${capitalizedEntrantType} ${capitalizedTicketType} Admission: $${(ticketPrice/100).toFixed(2)}\n`
     } else {
+
       receipt += `${capitalizedEntrantType} ${capitalizedTicketType} Admission: $${(ticketPrice/100).toFixed(2)} (${formattedExtras})\n`
     }
   }
